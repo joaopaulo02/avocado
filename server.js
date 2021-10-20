3b6a4a6560314a35671ef48bd8083b3111335a0c
joaopaulo02 <jpalonso0209@gmail.com>
2021-10-20 19:12:43 -0300

    first route created (/).

--- server.js
+++ server.js
@@ -2,5 +2,15 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3333
 
+// serve the css as static
+app.use(express.static('search_bar'))
+
+// enable req.body
+app.use(express.urlencoded({extended: true}))
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/home_page/home_page.html')
+})
+
 app.listen(PORT, () => console.log(`Server running at ${PORT}`))
 
