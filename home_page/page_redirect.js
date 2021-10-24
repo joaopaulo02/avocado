ada561d887bea9f51c019fac9e455fd043e304cd
joaopaulo02 <jpalonso0209@gmail.com>
2021-10-23 22:26:20 -0300

    functions created.

---
+++ home_page/page_redirect.js
@@ -0,0 +1,12 @@
+function signinSchoolRedirect() {
+  window.location.replace('localhost:3333/signin-school')
+}
+
+function enterRedirect() {
+  window.location.replace('localhost:3333/enter')
+}
+
+function budgetRedirect() {
+  window.location.replace('localhost:3333/budget')
+}
+
