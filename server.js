837283cd9423e8ae66c41e9b952e8cec5819f016
joaopaulo02 <jpalonso0209@gmail.com>
2021-11-15 21:50:53 -0300

    style permission added fo student_signed_in folder.

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ app.use(express.static('ranking_admin'))
 app.use(express.static('register_grades'))
 app.use(express.static('registered_grades'))
 app.use(express.static('signin_student'))
+app.use(express.static('student_signed_in'))
+
 
 
 
